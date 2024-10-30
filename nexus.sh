fae6e33a89f45c85e2f68cfa68690264c0c75689
0xGareeb <117015135+arun993@users.noreply.github.com>
2024-10-30 16:53:02 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -7,7 +7,7 @@ BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 PINK='\033[1;35m'
 
-#Multi wallet script just find and ** and replace all by account number
+#Multi wallet script just find and $Account_Number and replace all by account number
 
 show() {
     case $2 in
@@ -22,8 +22,13 @@ show() {
             ;;
     esac
 }
+# Prompt the user for the account number
+read -p "Enter number of the account: " Account_Number
 
-SERVICE_NAME="nexus**"
+# Create the directory
+mkdir "network-api$Account_Number"
+
+SERVICE_NAME="nexus$Account_Number"
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
 show "Installing Rust..." "progress"
@@ -48,19 +53,19 @@ else
     show "Git is already installed."
 fi
 
-if [ -d "$HOME/network-api**" ]; then
+if [ -d "$HOME/network-api$Account_Number" ]; then
     show "Deleting existing repository..." "progress"
-    rm -rf "$HOME/network-api**"
+    rm -rf "$HOME/network-api$Account_Number"
 fi
 sleep 3
 
 show "Cloning Nexus-XYZ network API repository..." "progress"
-if ! git clone https://github.com/nexus-xyz/network-api.git "$HOME/network-api**"; then
+if ! git clone https://github.com/nexus-xyz/network-api.git "$HOME/network-api$Account_Number"; then
     show "Failed to clone the repository." "error"
     exit 1
 fi
 
-cd $HOME/network-api**/clients/cli
+cd $HOME/network-api$Account_Number/clients/cli
 
 show "Installing required dependencies..." "progress"
 if ! sudo apt install pkg-config libssl-dev -y; then
@@ -68,25 +73,25 @@ if ! sudo apt install pkg-config libssl-dev -y; then
     exit 1
 fi
 
-if systemctl is-active --quiet nexus**.service; then
-    show "nexus**.service is currently running. Stopping and disabling it..."
-    sudo systemctl stop nexus**.service
-    sudo systemctl disable nexus**.service
+if systemctl is-active --quiet nexus$Account_Number.service; then
+    show "nexus$Account_Number.service is currently running. Stopping and disabling it..."
+    sudo systemctl stop nexus$Account_Number.service
+    sudo systemctl disable nexus$Account_Number.service
 else
-    show "nexus**.service is not running."
+    show "nexus$Account_Number.service is not running."
 fi
 
 show "Creating systemd service..." "progress"
 if ! sudo bash -c "cat > $SERVICE_FILE <<EOF
 [Unit]
-Description=Nexus XYZ Prover Service Instance **
+Description=Nexus XYZ Prover Service Instance $Account_Number
 After=network.target
 
 [Service]
 User=$USER
-WorkingDirectory=$HOME/network-api**/clients/cli
+WorkingDirectory=$HOME/network-api$Account_Number/clients/cli
 Environment=NONINTERACTIVE=1
-ExecStart=$HOME/.cargo/bin/cargo run --release --bin prover -- beta.orchestrator.nexus.xyz  --service-id=**
+ExecStart=$HOME/.cargo/bin/cargo run --release --bin prover -- beta.orchestrator.nexus.xyz  --service-id=$Account_Number
 Restart=always
 RestartSec=10
 
@@ -114,5 +119,5 @@ if ! sudo systemctl enable $SERVICE_NAME.service; then
 fi
 
 show "Nexus Prover installation and service setup complete!"
-show "You can check Nexus Prover logs using this command : journalctl -u nexus**.service -fn 50"
+show "You can check Nexus Prover logs using this command : journalctl -u nexus$Account_Number.service -fn 50"
 echo
